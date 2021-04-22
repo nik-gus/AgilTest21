8a485a6de4abb9f8ea596a1630672c56d854c017
nik-gus <niklas.gustafsson@gmail.com>
2021-04-22 11:16:50 +0200

    added Cucumber structure

---
+++ src/test/java/stepdefinitions/DemoSteps.java
@@ -0,0 +1,8 @@
+//  Den fil existerar bara för att det skulle gå att pusha upp detta package till github
+
+package stepdefinitions;
+
+public class DemoSteps {
+
+
+}
---
+++ src/test/resources/features/demo.feature
@@ -0,0 +1,2 @@
+# den här filen existerar bara för att det skulle gå att pusha upp foldern
+#  ska raderas
