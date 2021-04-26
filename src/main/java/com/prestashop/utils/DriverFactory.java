394eaca6e2be1703f0b8bfec7677acb3be937e5d
nik-gus <niklas.gustafsson@gmail.com>
2021-04-26 15:43:28 +0200

    Modified DriverFactory to handle ChromeDriver and FirefoxDriver under Windows and Mac

--- src/main/java/com/prestashop/utils/DriverFactory.java
+++ src/main/java/com/prestashop/utils/DriverFactory.java
@@ -2,6 +2,7 @@ package com.prestashop.utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class DriverFactory {
@@ -9,22 +10,59 @@ public class DriverFactory {
     private static WebDriver driver;
     private static WebDriverWait wait;
 
+    private static final String MAC = "mac";
+    private static final String WIN = "win";
+
+    private static final String GECKO_PROPERTY = "webdriver.gecko.driver";
+    private static final String CHROME_PROPERTY = "webdriver.chrome.driver";
+
+    private static final String GECKO_MAC_PATH_FILE = "src/main/driverStore/geckodriver_mac";
+    private static final String CHROME_MAC_PATH_FILE = "src/main/driverStore/chromedriver_mac";
+
+    private static final String GECKO_WIN_PATH_FILE = "src/main/driverStore/geckodriver_win.exe";
+    private static final String CHROME_WIN_PATH_FILE = "src/main/driverStore/chromedriver_win.exe";
+
     private DriverFactory() {
         //prevent instantiation
     }
 
-
     public static WebDriver getDriver() {
-        if (driver == null) {
-            System.setProperty("webdriver.chrome.driver","C:\\WebDriver\\bin\\chromedriver.exe");
-            driver = new ChromeDriver();
+        if (driver != null) {
+            return driver;
         }
-        return driver;
-    }
+
+        String driverProperty = System.getProperty("WebDriver");
+
+        if (driverProperty == null)  {
+            driver = new ChromeDriver(); //HÄR BEHÖVS System Set property etc
+            return driver;
+        }
+
+            switch (driverProperty) {
+                case "Chrome":
+                    if (System.getProperty("os.name").toLowerCase().contains(MAC)) {
+                        System.setProperty(CHROME_PROPERTY, CHROME_MAC_PATH_FILE);
+                    } else if (System.getProperty("os.name").toLowerCase().contains(WIN)) {
+                        System.setProperty(CHROME_PROPERTY, CHROME_WIN_PATH_FILE);
+                    }
+                    driver = new ChromeDriver();
+                    return driver;
+                case "Firefox":
+                    if (System.getProperty("os.name").toLowerCase().contains(MAC)) {
+                        System.setProperty(GECKO_PROPERTY, GECKO_MAC_PATH_FILE);
+                    } else if (System.getProperty("os.name").toLowerCase().contains(WIN)) {
+                        System.setProperty(GECKO_PROPERTY, GECKO_WIN_PATH_FILE);
+                    }
+                    driver = new FirefoxDriver();
+                    return driver;
+                default:
+                    throw new IllegalArgumentException("No '" + driver + "' driver available.");
+            }
+        }
 
     public static WebDriverWait getWebDriverWait() {
         if (wait == null) {
-            wait = new WebDriverWait(getDriver(), 5);
+            wait = new WebDriverWait(getDriver(), 10);
         }
         return wait;
     }
