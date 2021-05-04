fcb5f4a5f2ac9af7aa5721087c8f3d3073eee07c
HarkanSingh <75072616+HarkanSingh@users.noreply.github.com>
2021-05-04 17:32:26 +0200

    Merge remote-tracking branch 'origin/main' into main

---
+++ src/test/java/stepdefinitions/SortAcess.java
@@ -0,0 +1,80 @@
+package stepdefinitions;
+
+import com.prestashop.pages.accessories.AccessoriesPage;
+import com.prestashop.pages.cart.CartPage;
+import com.prestashop.pages.order.OrderPage;
+import com.prestashop.pages.top.TopMenu;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static com.prestashop.pages.accessories.AccessoriesPage.getAccessoriesPage;
+import static com.prestashop.pages.cart.CartPage.getCartPage;
+import static com.prestashop.pages.order.OrderPage.getOrderPage;
+import static com.prestashop.utils.DriverFactory.getDriver;
+import static com.prestashop.utils.DriverFactory.getWebDriverWait;
+
+public class SortAcess {
+
+
+    public static WebDriver driver;
+    public static WebDriverWait wait;
+
+    TopMenu top = TopMenu.getTopMenu();
+    AccessoriesPage accessories = getAccessoriesPage();
+    CartPage cart = getCartPage();
+    OrderPage order = getOrderPage();
+
+    @Before
+    public void setup() {
+        driver = getDriver();
+        wait = getWebDriverWait();
+        driver.get("http://40.76.27.113:8085/en/");
+    }
+
+
+
+    @Given(": User is on Accessories page")
+    public void user_is_on_accessories_page() {
+        top.act()
+                .selectAccessories()
+                .andThen()
+                .verifyAccessoriesPage("Accessories");
+    }
+    @When(": User chooses to sort products by A To Z")
+    public void user_chooses_to_sort_products_by_a_to_z() {
+        accessories.act()
+                .ClickRelevenceButton()
+                .ClickNameAToZ();
+    }
+    @Then("Success message is displayed")
+    public void success_message_is_displayed() {
+        System.out.println("Sorted by AtoZ successfull");
+
+    }
+
+    @When(": User chooses to sort products by Z To A")
+    public void userChoosesToSortProductsByZToA() {
+        accessories.act()
+                .ClickRelevenceButton()
+                .ClickNameZToA();
+    }
+
+    @When(": User chooses to sort products by Low To High")
+    public void userChoosesToSortProductsByLowToHigh() {
+        accessories.act()
+                .ClickRelevenceButton()
+                .ClickLowToHigh();
+    }
+
+    @When(": User chooses to sort products by High To Low")
+    public void userChoosesToSortProductsByHighToLow() {
+        accessories.act()
+                .ClickRelevenceButton()
+                .ClickHighToLow();
+    }
+}
