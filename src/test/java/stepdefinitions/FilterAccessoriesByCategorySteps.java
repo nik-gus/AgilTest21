a10be889d5e182434f4633fb062404d04552edf5
nik-gus <niklas.gustafsson@gmail.com>
2021-05-06 23:30:49 +0200

    cleaning of FilterAccessoriesByCategorySteps

--- src/test/java/stepdefinitions/FilterAccessoriesByCategorySteps.java
+++ src/test/java/stepdefinitions/FilterAccessoriesByCategorySteps.java
@@ -13,29 +13,22 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import static com.prestashop.pages.accessories.AccessoriesPage.getAccessoriesPage;
 import static com.prestashop.pages.cart.CartPage.getCartPage;
 import static com.prestashop.pages.order.OrderPage.getOrderPage;
-import static com.prestashop.pages.top.TopMenu.getTopMenu;
 
 public class FilterAccessoriesByCategorySteps {
-    public static WebDriver driver;
-    public static WebDriverWait wait;
 
     TopMenu top = TopMenu.getTopMenu();
     AccessoriesPage accessories = getAccessoriesPage();
-    CartPage cart = getCartPage();
-    OrderPage order = getOrderPage();
 
     @Given(": User is on Accessorie page")
     public void userIsOnAccessoriePage() {
         top.act()
                 .selectAccessories();
-
     }
 
     @When(": User chooses to filter by Home")
     public void userChoosesToFilterByHome() {
         accessories.act()
                 .filterByHome();
-
     }
 
     @Then("Successs message is displayed Filter by Home Filtered")
@@ -44,12 +37,10 @@ public class FilterAccessoriesByCategorySteps {
                 .verifyCategoryByHome();
     }
 
-
     @When(": User chooses to filter by Stationery")
     public void userChoosesToFilterByStationery() {
         accessories.act()
                 .filterByStationery();
-
     }
 
     @Then("Successs message is displayed Filter by Stationery Filtered")
@@ -62,9 +53,7 @@ public class FilterAccessoriesByCategorySteps {
     public void userChoosesToFilterByColourBlack() {
         accessories.act()
                 .filterByColourBlack();
-
     }
-
 
     @Then("Successs message is displayed Filter by Black Colour Filtered")
     public void successsMessageIsDisplayedFilterByBlackColourFiltered() {
@@ -72,42 +61,36 @@ public class FilterAccessoriesByCategorySteps {
                 .verifyCategoryByColourBlack();
     }
 
-
-
     @When(": User chooses to filter by Colour White")
     public void userChoosesToFilterByColourWhite() {
         accessories.act()
                 .filterByColourWhite();
-
     }
+
     @Then("Successs message is displayed Filter by White Colour Filtered")
     public void successsMessageIsDisplayedFilterByWhiteColourFiltered() {
         accessories.verify()
                 .verifyCategoryByColourWhite();
     }
 
-
-
-
     @When(": User chooses to filter by Ceramic")
     public void userChoosesToFilterByCeramic() {
         accessories.act()
                 .filterByCeramic();
-
     }
+
     @Then("Successs message is displayed Filter by Ceramic Option Filtered")
     public void successsMessageIsDisplayedFilterByCeramicOptionFiltered() {
         accessories.verify()
                 .verifyCategoryByCeramic();
     }
 
-
     @When(": User chooses to filter by Polyester")
     public void userChoosesToFilterByPolyester() {
         accessories.act()
                 .filterByPolyester();
-
     }
+
     @Then("Successs message is displayed Filter by Polyester Option Filtered")
     public void successsMessageIsDisplayedFilterByPolyesterOptionFiltered() {
         accessories.verify()
@@ -118,7 +101,6 @@ public class FilterAccessoriesByCategorySteps {
     public void userChoosesToFilterByRecycle() {
         accessories.act()
                 .filterByRecycle();
-
     }
 
     @Then("Successs message is displayed Filter by Recycle Option Filtered")
@@ -127,40 +109,34 @@ public class FilterAccessoriesByCategorySteps {
                 .verifyCategoryByRecycle();
     }
 
-
-
     @When(": User chooses to filter by {int} pages")
     public void userChoosesToFilterByPages(int arg0) {
         accessories.act()
                 .filterByProperty120Pages();
-
     }
+
     @Then("Successs message is displayed Filter by {int} Pages Option Filtered")
     public void successsMessageIsDisplayedFilterByPagesOptionFiltered(int arg0) {
         accessories.verify()
                 .verifyCategoryBy120Pages();
     }
 
-
-
     @When(": User chooses to filter by Removable")
     public void userChoosesToFilterByRemovable() {
         accessories.act()
                 .filterByPropertyRemovable();
-
     }
+
     @Then("Successs message is displayed Filter by Removable Option Filtered")
     public void successsMessageIsDisplayedFilterByRemovableOptionFiltered() {
         accessories.verify()
                 .verifyCategoryByRemovable();
     }
 
-
     @When(": User chooses to filter by Graphic")
     public void userChoosesToFilterByGraphic() {
         accessories.act()
                 .filterByPropertyByBrandGraphic();
-
     }
 
     @Then("Successs message is displayed Filter by Graphic Brand Option Filtered")
@@ -173,7 +149,6 @@ public class FilterAccessoriesByCategorySteps {
     public void userChoosesToFilterByStudio() {
         accessories.act()
                 .filterByPropertyByBrandStudio();
-
     }
 
     @Then("Successs message is displayed Filter by Studio Brand Option Filtered")
@@ -181,4 +156,5 @@ public class FilterAccessoriesByCategorySteps {
         accessories.verify()
                 .verifyCategoryByBrandStudio();
     }
+
 }
