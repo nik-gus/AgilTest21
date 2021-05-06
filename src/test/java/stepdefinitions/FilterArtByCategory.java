d4af7a5e8c48d9c0e57cc43ef3ff57f5d3981579
HarkanSingh <75072616+HarkanSingh@users.noreply.github.com>
2021-05-06 23:02:22 +0200

    Merge remote-tracking branch 'origin/main' into main

--- src/test/java/stepdefinitions/FilterArtByCategory.java
+++ src/test/java/stepdefinitions/FilterArtByCategory.java
@@ -35,20 +35,25 @@ public class FilterArtByCategory {
     public void userChoosesToFilterByHome() {
         accessories.act()
                 .filterByHome();
+
+    }
+
+    @Then("Successs message is displayed Filter by Home Filtered")
+    public void successsMessageIsDisplayedFilterByHomeFiltered() {
         accessories.verify()
                 .verifyCategoryByHome();
     }
 
-    @Then("Successs message is displayed")
-    public void successsMessageIsDisplayed() {
-        System.out.println("Sorted is successfull");
-
-    }
 
     @When(": User chooses to filter by Stationery")
     public void userChoosesToFilterByStationery() {
         accessories.act()
                 .filterByStationery();
+
+    }
+
+    @Then("Successs message is displayed Filter by Stationery Filtered")
+    public void successsMessageIsDisplayedFilterByStationeryFiltered() {
         accessories.verify()
                 .verifyCategoryByStationery();
     }
@@ -57,30 +62,54 @@ public class FilterArtByCategory {
     public void userChoosesToFilterByColourBlack() {
         accessories.act()
                 .filterByColourBlack();
+
+    }
+
+
+    @Then("Successs message is displayed Filter by Black Colour Filtered")
+    public void successsMessageIsDisplayedFilterByBlackColourFiltered() {
         accessories.verify()
                 .verifyCategoryByColourBlack();
     }
+
+
 
     @When(": User chooses to filter by Colour White")
     public void userChoosesToFilterByColourWhite() {
         accessories.act()
                 .filterByColourWhite();
+
+    }
+    @Then("Successs message is displayed Filter by White Colour Filtered")
+    public void successsMessageIsDisplayedFilterByWhiteColourFiltered() {
         accessories.verify()
                 .verifyCategoryByColourWhite();
     }
+
+
+
 
     @When(": User chooses to filter by Ceramic")
     public void userChoosesToFilterByCeramic() {
         accessories.act()
                 .filterByCeramic();
+
+    }
+    @Then("Successs message is displayed Filter by Ceramic Option Filtered")
+    public void successsMessageIsDisplayedFilterByCeramicOptionFiltered() {
         accessories.verify()
                 .verifyCategoryByCeramic();
     }
+
 
     @When(": User chooses to filter by Polyester")
     public void userChoosesToFilterByPolyester() {
         accessories.act()
                 .filterByPolyester();
+
+    }
+    @Then("Successs message is displayed Filter by Polyester Option Filtered")
+    public void successsMessageIsDisplayedFilterByPolyesterOptionFiltered() {
         accessories.verify()
                 .verifyCategoryByPolyester();
     }
@@ -89,30 +118,53 @@ public class FilterArtByCategory {
     public void userChoosesToFilterByRecycle() {
         accessories.act()
                 .filterByRecycle();
+
+    }
+
+    @Then("Successs message is displayed Filter by Recycle Option Filtered")
+    public void successsMessageIsDisplayedFilterByRecycleOptionFiltered() {
         accessories.verify()
                 .verifyCategoryByRecycle();
     }
+
+
 
     @When(": User chooses to filter by {int} pages")
     public void userChoosesToFilterByPages(int arg0) {
         accessories.act()
                 .filterByProperty120Pages();
+
+    }
+    @Then("Successs message is displayed Filter by {int} Pages Option Filtered")
+    public void successsMessageIsDisplayedFilterByPagesOptionFiltered(int arg0) {
         accessories.verify()
                 .verifyCategoryBy120Pages();
     }
+
+
 
     @When(": User chooses to filter by Removable")
     public void userChoosesToFilterByRemovable() {
         accessories.act()
                 .filterByPropertyRemovable();
+
+    }
+    @Then("Successs message is displayed Filter by Removable Option Filtered")
+    public void successsMessageIsDisplayedFilterByRemovableOptionFiltered() {
         accessories.verify()
                 .verifyCategoryByRemovable();
     }
+
 
     @When(": User chooses to filter by Graphic")
     public void userChoosesToFilterByGraphic() {
         accessories.act()
                 .filterByPropertyByBrandGraphic();
+
+    }
+
+    @Then("Successs message is displayed Filter by Graphic Brand Option Filtered")
+    public void successsMessageIsDisplayedFilterByGraphicBrandOptionFiltered() {
         accessories.verify()
                 .verifyCategoryByBrandGraphic();
     }
@@ -121,6 +173,11 @@ public class FilterArtByCategory {
     public void userChoosesToFilterByStudio() {
         accessories.act()
                 .filterByPropertyByBrandStudio();
+
+    }
+
+    @Then("Successs message is displayed Filter by Studio Brand Option Filtered")
+    public void successsMessageIsDisplayedFilterByStudioBrandOptionFiltered() {
         accessories.verify()
                 .verifyCategoryByBrandStudio();
     }
--- src/test/resources/features/FilterByCategory.feature
+++ src/test/resources/features/FilterByCategory.feature
@@ -3,54 +3,54 @@ Feature: Filter by Category
   Scenario: User Filters by Home Option
     Given : User is on Accessorie page
     When  : User chooses to filter by Home
-    Then Successs message is displayed
+    Then Successs message is displayed Filter by Home Filtered
 
   Scenario: User Filters by Stationery Option
     Given : User is on Accessorie page
     When  : User chooses to filter by Stationery
-    Then Successs message is displayed
+    Then Successs message is displayed Filter by Stationery Filtered
 
   Scenario: User Filters by Colour Black Option
     Given : User is on Accessorie page
     When  : User chooses to filter by Colour Black
-    Then Successs message is displayed
+    Then Successs message is displayed Filter by Black Colour Filtered
 
   Scenario: User Filters by Colour White Option
     Given : User is on Accessorie page
     When  : User chooses to filter by Colour White
-    Then Successs message is displayed
+    Then Successs message is displayed Filter by White Colour Filtered
 
   Scenario: User Filters by Composition, Ceramic Option
     Given : User is on Accessorie page
     When  : User chooses to filter by Ceramic
-    Then Successs message is displayed
+    Then Successs message is displayed Filter by Ceramic Option Filtered
 
   Scenario: User Filters by Composition, Polyester Option
     Given : User is on Accessorie page
     When  : User chooses to filter by Polyester
-    Then Successs message is displayed
+    Then Successs message is displayed Filter by Polyester Option Filtered
 
   Scenario: User Filters by Composition, Recycle Option
     Given : User is on Accessorie page
     When  : User chooses to filter by Recycle
-    Then Successs message is displayed
+    Then Successs message is displayed Filter by Recycle Option Filtered
 
   Scenario: User Filters by Property, 120 pages Option
     Given : User is on Accessorie page
     When  : User chooses to filter by 120 pages
-    Then Successs message is displayed
+    Then Successs message is displayed Filter by 120 Pages Option Filtered
 
   Scenario: User Filters by Property, Removable cover Option
     Given : User is on Accessorie page
     When  : User chooses to filter by Removable
-    Then Successs message is displayed
+    Then Successs message is displayed Filter by Removable Option Filtered
 
   Scenario: User Filters by Property, Brand Option
     Given : User is on Accessorie page
     When  : User chooses to filter by Graphic
-    Then Successs message is displayed
+    Then Successs message is displayed Filter by Graphic Brand Option Filtered
 
   Scenario: User Filters by Property, Brand Option
     Given : User is on Accessorie page
     When  : User chooses to filter by Studio
-    Then Successs message is displayed
+    Then Successs message is displayed Filter by Studio Brand Option Filtered
