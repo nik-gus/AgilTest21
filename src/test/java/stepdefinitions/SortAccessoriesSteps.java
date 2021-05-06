ffd63f887284b6f284f81cf2e021e484adf2e509
nik-gus <niklas.gustafsson@gmail.com>
2021-05-06 22:33:05 +0200

    renamed SortAccess to SortAccessoriesSteps, and some clean-up of code

--- src/test/java/stepdefinitions/SortAccessoriesSteps.java
+++ src/test/java/stepdefinitions/SortAccessoriesSteps.java
@@ -4,7 +4,6 @@ import com.prestashop.pages.accessories.AccessoriesPage;
 import com.prestashop.pages.cart.CartPage;
 import com.prestashop.pages.order.OrderPage;
 import com.prestashop.pages.top.TopMenu;
-import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -18,16 +17,13 @@ import static com.prestashop.pages.order.OrderPage.getOrderPage;
 import static com.prestashop.utils.DriverFactory.getDriver;
 import static com.prestashop.utils.DriverFactory.getWebDriverWait;
 
-public class SortAcess {
-
+public class SortAccessoriesSteps {
 
     public static WebDriver driver;
     public static WebDriverWait wait;
 
     TopMenu top = TopMenu.getTopMenu();
     AccessoriesPage accessories = getAccessoriesPage();
-    CartPage cart = getCartPage();
-    OrderPage order = getOrderPage();
 
     @Before
     public void setup() {
@@ -36,8 +32,6 @@ public class SortAcess {
         driver.get("http://40.76.27.113:8085/en/");
     }
 
-
-
     @Given(": User is on Accessories page")
     public void user_is_on_accessories_page() {
         top.act()
@@ -45,6 +39,7 @@ public class SortAcess {
                     .andThen()
                 .verifyPageTitle("Accessories");
     }
+
     @When(": User chooses to sort products by A To Z")
     public void user_chooses_to_sort_products_by_a_to_z() {
         accessories.act()
@@ -76,7 +71,6 @@ public class SortAcess {
         accessories.act()
                 .clickRelevenceButton()
                 .clickLowToHigh();
-
     }
 
     @When(": User chooses to sort products by High To Low")
@@ -84,7 +78,6 @@ public class SortAcess {
         accessories.act()
                 .clickRelevenceButton()
                 .clickHighToLow();
-
     }
 
     @Then("Success message is displayed When Low To High is verified")
@@ -98,4 +91,5 @@ public class SortAcess {
         accessories.verify()
                 .verifyHighToLow();
     }
+
 }
