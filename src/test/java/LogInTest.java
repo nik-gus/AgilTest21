92f896d91d800891a6eb45a3b2236719cc112c38
nik-gus <niklas.gustafsson@gmail.com>
2021-05-07 02:48:41 +0200

    test notifying Teams

--- src/test/java/LogInTest.java
+++ src/test/java/LogInTest.java
@@ -20,7 +20,7 @@ class LogInTest extends BaseTestClass {
                 .enterPassword("tolvan1")
                 .signIn();
         top.verify()
-                .userLoggedIn("tttolvan tolvansson");
+                .userLoggedIn("tttttolvan tolvansson");
     }
 
 }
