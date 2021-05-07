e7a00c237a37eef5b17c6cfdd72e3c5334288af4
nik-gus <niklas.gustafsson@gmail.com>
2021-05-07 18:42:56 +0200

    added DemoTest

---
+++ src/test/java/DemoTest.java
@@ -0,0 +1,23 @@
+import com.prestashop.pages.clothes.ClothesPage;
+import com.prestashop.pages.clothes.Size;
+import com.prestashop.pages.top.TopMenu;
+import org.junit.jupiter.api.Test;
+
+ class DemoTest extends BaseTestClass{
+
+    TopMenu top = TopMenu.getTopMenu();
+    ClothesPage clothes = ClothesPage.getClothesPage();
+
+    @Test
+    void testFilterBlaBla() {
+        top.act()
+                .selectClothes();
+        clothes.act()
+                .selectMen()
+                .filterSize("M")
+                .filterSize("L")
+                .filterSize("XL")
+                .filterSize("S");
+    }
+
+}
